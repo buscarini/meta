609338ee6290aec2c58853982d2e7febf2eaab90
José Manuel Sánchez <buscarini@gmail.com>
2014-05-12 11:29:51 +0200

    Added platform sql for model product

---
+++ example_spec/platforms/sql/GlobalPlatform.py
@@ -0,0 +1,43 @@
+import os
+import json
+from meta.MetaProcessor import MetaProcessor
+
+class GlobalPlatform(MetaProcessor):
+    """docstring for Preprocessor"""
+    def __init__(self,config,stringUtils):
+        super(GlobalPlatform, self).__init__(config,stringUtils)
+
+        thisPath = os.path.realpath(__file__)
+        
+        self.globalsPath = os.path.join(os.path.dirname(thisPath),'globals.json')
+
+    def preprocess(self,hash,hashes):
+        """Make any preprocessing necessary for the platform"""
+        return self.addHashGlobals(hash)
+
+    def addHashGlobals(self,hashDic):
+        """docstring for addHashGlobals"""
+
+        with open (self.globalsPath, "r") as file:
+            globalsString = file.read()
+            globalsDic = json.loads(globalsString)
+
+        hashDic['_globals_'] = globalsDic
+
+        return hashDic
+        
+    def platformTypeForType(self,type):
+        if type=='string':
+            return 'VARCHAR(500)'
+        elif type=='integer':
+            return 'INT'
+        elif type=='float':
+            return 'FLOAT'
+        elif type=='double':
+            return 'DOUBLE'
+        elif type=='bool':
+            return 'BOOLEAN'
+        elif type=='date':
+            return 'TIMESTAMP'
+        else:
+            return None
---
+++ templates/model/platforms/sql/Platform.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import json
+
+from meta.MetaProcessor import MetaProcessor
+
+class Platform(MetaProcessor):
+    """docstring for Platform"""
+        
+    def preprocess_property(self,property,hash,hashes):
+        """docstring for preprocess_property"""
+        property['_camelcase_'] = self.stringUtils.camelcase(str(property['name']))
+        property['_capitalized_'] = self.stringUtils.capitalize(str(property['name']))
+        
+        type = property['type']
+        property['type_' + type] = True
+        
+        platformType = self.globalPlatform.platformTypeForType(type)
+        if platformType!=None:
+            property['type'] = platformType
+        else:
+            print("Error: unknown property type: " + type)
+            sys.exit()
+            
+    def preprocess(self,hash,hashes):
+        if hash!=None and 'properties' in hash:
+            i=0
+            properties = hash['properties']
+            for property in properties:
+                self.preprocess_property(property,hash,hashes)
+                i=i+1
+            
+            self.preprocessList(properties)
+            
+        if hash!=None and 'primaryKeys' in hash:
+            self.preprocessList(hash['primaryKeys'])
+            
+    def finalFileName(self,fileName,hash):
+        """docstring for finalFileName"""
+
+        entityName = None
+        if hash!=None and 'entityName' in hash:
+            entityName = hash['entityName']
+        if (entityName):
+            fileName = fileName.replace("entity",entityName)
+        
+        return fileName
